8a8465ea398a0d1f17b4aec276ce46a8a8230004
Suchitharathod <bhukyauschitharathod@gmail.com>
2022-07-03 00:03:38 +0530

    new features

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@
 import './App.css';
 import Row  from "./Row";
 import requests from "./requests";
+import Banner from "./Banner";
+import Nav from "./nav";
 
 function App() {
   return (
     <div className="App">
-    <h1>Building netflix clone.</h1>
-    <Row title="netflix  originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
-    <Row title="Trending  now" fetchUrl={requests.fetchTrending} />
+    <Nav />
+    <Banner fetchUrl={requests.fetchTrending}/>
+    <Row title="Netflix  Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
+    <Row title="Trending  Now" fetchUrl={requests.fetchTrending} />
     <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
     <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
     <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
